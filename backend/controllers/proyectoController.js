980941401c6bebf6c293511ab8cff3f0c68e1f5d
Alberto Valido <albertovalidofuentes2001@gmail.com>
2022-10-16 18:36:46 +0200

    Creada estructura del controlador de proyectos

---
+++ backend/controllers/proyectoController.js
@@ -0,0 +1,50 @@
+// * Obtenemos los proyectos del usuario autenticado
+const obtenerProyectos = async (req, res) => {
+
+}
+
+// * Para crear nuevos proyectos
+const nuevoProyecto = async (req, res) => {
+
+}
+
+// * Lista un proyecto y las tareas asociadas
+const obtenerProyecto = async (req, res) => {
+
+}
+
+// * Editar un proyecto existente
+const editarProyecto = async (req, res) => {
+
+}
+
+// * Eliminamos un proyecto
+const eliminarProyecto = async (req, res) => {
+
+}
+
+// * Agregamos un colaborador
+const agregarColaborador = async (req, res) => {
+
+}
+
+// * Eliminamo un colaborador
+const eliminarColaborador = async (req, res) => {
+
+}
+
+// * Obtenemos las tareas
+const obtenerTareas = async (req, res) => {
+
+}
+
+export {
+    obtenerProyectos,
+    nuevoProyecto,
+    obtenerProyecto,
+    editarProyecto,
+    eliminarProyecto,
+    agregarColaborador,
+    eliminarColaborador,
+    obtenerTareas
+}
