084d4167d07f698d513a685cf9f262c6f3bda779
Alberto Valido <albertovalidofuentes2001@gmail.com>
2022-10-26 20:38:34 +0200

    Agrego mas rutas para los proyectos

--- backend/routes/proyectoRoutes.js
+++ backend/routes/proyectoRoutes.js
@@ -22,14 +22,18 @@ const router = express.Router()
 
 router
     .route('/')
-    .get(checkout, obtenerProyectos)
-    .post(checkout, nuevoProyecto)
+    .get(checkAuth, obtenerProyectos)
+    .post(checkAuth, nuevoProyecto)
 
 router
     .route('/:id')
     .get(checkAuth, obtenerProyecto)
     .put(checkAuth, editarProyecto)
     .delete(checkAuth, eliminarProyecto)
+
+router.get('/tareas/:id', checkAuth, obtenerTareas)
+router.post('/agregar-colaborador/:id', checkAuth, agregarColaborador)
+router.post('/agregar-colaborador/:id', checkAuth, eliminarColaborador)
 
 
 
